948c59db50a48845d98dfa4c627aab733344dfe9
GSayuriNHeldt <gabriela.sayuri-ninomiya-heldt@epitech.eu>
2025-03-03 16:42:05 +0100

    feat: getAllWorspace functions

--- front-end/src/app/services/crud/crud.service.ts
+++ front-end/src/app/services/crud/crud.service.ts
@@ -1,42 +1,59 @@
-import { Injectable, inject, signal } from '@angular/core'; import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../auth/auth.service';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../services/auth/auth.service';
 import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-export class DataTestService {
+export class GetService {
   private readonly _authService = inject(AuthService);
-  private apiTrello = "https://api.trello.com/1";
-  private apiKeyToken = `&key=${environment.apiKey}&token=${this._authService.getToken()}`;
+  private apiTrello = 'https://api.trello.com/1';
   private http = inject(HttpClient);
 
-  getAllWorkspace(membersObject: Object) {
-    return this.getAll('workspaces', membersObject);
+  private getApiKeyToken(): string {
+    return `&key=${environment.apiKey}&token=${this._authService.getToken()}`;
   }
+  getAllWorkspace(): Observable<any> {
+    return new Observable((observer) => {
+      this.getMemberId().subscribe((data: any) => {
+        if (data) {
+          const memberId = Object.values(data)[0];
+          this.getAll('organizations', { members: memberId }).subscribe(
+            (organizations) => {
+              observer.next(organizations);
+              observer.complete();
+            },
+            (error) => observer.error(error)
+          );
+        }
+      });
+    });
+  }
+
   /*membersObject={
     members: {id};
     }*/
 
   getAllBoards(organizationsMembersObject: Object) {
-    return this.getAll('boards', organizationsMemberObject)
+    return this.getAll('boards', organizationsMembersObject);
   }
   // organizationsMemberObject={
   //   organizations: {idOrganization};
   // } || {
   // members: {idMember}
-// }
+  // }
 
-  getAllLists(boardsObject:Object){
-    return this.getAll('lists',boardsObject);
+  getAllLists(boardsObject: Object) {
+    return this.getAll('lists', boardsObject);
   }
   //boardsObject={
   //   boards: {idBoard};
   // }
 
-  getAllCards(memberBoardOrListObject:Object){
-    return this.getAll('cards',memberBoardOrListObject);
+  getAllCards(memberBoardOrListObject: Object): Observable<Object> {
+    return this.getAll('cards', memberBoardOrListObject);
   }
   /*
   memberBoardOrListObject={
@@ -47,13 +64,16 @@ export class DataTestService {
     lists:{idList}
     }
    */
-  getAll(typeQuest: String, fromWhatTable: Object) {
+  getAll(typeQuest: String, fromWhatTable: Object): Observable<Object> {
     let uri = this.apiTrello;
-    uri += `/${Object.keys(fromWhatTable)[0]}/
-            ${Object.values(fromWhatTable)[0]}/
-            ${typeQuest}?`;
-    uri += this.apiKeyToken;
-    return this.http.get(uri);
+    uri += `/${Object.keys(fromWhatTable)[0]}/${Object.values(fromWhatTable)[0]}/${typeQuest}?`;
+    uri += this.getApiKeyToken();
+    return this.http.get(uri, {
+      responseType: 'json',
+    });
+  }
+
+  getMemberId(): Observable<Object> {
+    return this.getAll('member', { tokens: this._authService.getToken() });
   }
 }
-
