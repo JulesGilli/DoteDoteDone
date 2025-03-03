bf6600f43d277f2e75524274dae166caa2ec9a9f
JulesGilli <jules.gilli@epitech.eu>
2025-03-03 15:56:22 +0100

    fix: fix on visual + clean code

--- front-end/src/app/pages/all-cards/all-cards.component.ts
+++ front-end/src/app/pages/all-cards/all-cards.component.ts
@@ -1,20 +1,25 @@
-import { Component, HostListener } from '@angular/core';
-import {CardComponent} from '../../components/card/card.component';
-import {NgForOf, NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { NgForOf, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-cards',
   templateUrl: './all-cards.component.html',
+  standalone: true,
   imports: [
     CardComponent,
+    ModalComponent,
     NgForOf,
+    NgIf,
     FormsModule
   ],
   styleUrls: ['./all-cards.component.scss']
 })
 export class AllCardsComponent {
   selectedWorkspace = 'workspace1';
+  selectedTicket: any = null;
 
   tickets = [
     { titre: 'Card name', statusCard: 'normal', ticketId: 'DEV-01', manager: 'username' },
@@ -25,4 +30,12 @@ export class AllCardsComponent {
     { titre: 'Card name', statusCard: 'minor', ticketId: 'DEV-06', manager: 'username' },
     { titre: 'Card name', statusCard: 'blocking', ticketId: 'DEV-07', manager: 'username' }
   ];
+
+  openModal(ticket: any) {
+    this.selectedTicket = ticket;
+  }
+
+  closeModal() {
+    this.selectedTicket = null;
+  }
 }
