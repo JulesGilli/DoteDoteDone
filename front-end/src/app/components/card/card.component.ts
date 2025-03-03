f52d51f7c3e1d8d798155dbddd6bf0eaf8e1bc3c
JulesGilli <jules.gilli@epitech.eu>
2025-03-03 15:55:32 +0100

    fix: make clickable card

--- front-end/src/app/components/card/card.component.ts
+++ front-end/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -11,4 +11,10 @@ export class CardComponent {
   @Input() statusCard!: string;
   @Input() ticketId!: string;
   @Input() manager!: string;
+  @Output() cardClick = new EventEmitter<void>();
+
+  onTitleClick(event: Event) {
+    event.stopPropagation();
+    this.cardClick.emit();
+  }
 }
