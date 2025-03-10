121044d1b54bf1a11bf6a816abbe978666051853
JulesGilli <jules.gilli@epitech.eu>
2025-03-10 11:03:16 +0100

    fix: visual rendering fix nav-bar unfolding

--- front-end/src/app/components/navbar/navbar.component.ts
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   isExpanded = false;
