d9354d7fb9625d6ef9a70f6773b2d0bc373b2ce5
ileanasaintpaul <ileana.saint-paul@epitech.eu>
2025-03-10 14:44:56 +0100

    feat: display on select name of workspaces and boards

--- front-end/src/app/pages/kanban/kanban.component.ts
+++ front-end/src/app/pages/kanban/kanban.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { ListComponent } from '../../components/list/list.component';
+import {Board, Workspace} from '../../models';
+import {GetService} from '../../services';
+import {SharedModule} from '../../../share.module';
 
 @Component({
   selector: 'app-kanban',
-  imports: [ListComponent],
+  imports: [ListComponent, SharedModule],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss',
 })
-export class KanbanComponent {}
+export class KanbanComponent implements OnInit {
+  private readonly _getService = inject(GetService);
+  selectedWorkspace!: string | undefined;
+  workspaces: Workspace[] = [];
+  boards: Board[] = [];
+  selectedBoard!: string | undefined;
+
+
+  ngOnInit() {
+    this._getService.getAllWorkspace().subscribe((data: Workspace[]) => {
+      this.workspaces = data;
+      if (this.workspaces.length > 0) {
+        this.selectedWorkspace = this.workspaces[0].displayName;
+
+        this._getService.getAllBoards({organizations: this.workspaces[0].id}).subscribe((data: Board[]) => {
+          this.boards = data;
+          if (this.boards.length > 0) {
+            this.selectedBoard = this.boards[0].name;
+          }
+        })
+      }
+    });
+  }
+}
