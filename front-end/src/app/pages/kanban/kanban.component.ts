54ff776ed3ed4ccb1df0cf3739f2b497ae44c5f8
ileanasaintpaul <ileana.saint-paul@epitech.eu>
2025-03-11 15:09:57 +0100

    feat: changes the board value of select according to the workspace selected

--- front-end/src/app/pages/kanban/kanban.component.ts
+++ front-end/src/app/pages/kanban/kanban.component.ts
@@ -1,36 +1,41 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ListComponent } from '../../components/list/list.component';
-import {Board, Workspace} from '../../models';
-import {GetService} from '../../services';
-import {SharedModule} from '../../../share.module';
+import { Board, Workspace } from '../../models';
+import { GetService } from '../../services';
+import { SharedModule } from '../../../shared.module';
 
 @Component({
   selector: 'app-kanban',
   imports: [ListComponent, SharedModule],
-  templateUrl: './kanban.component.html',
+  templateUrl: 'kanban.component.html',
   styleUrl: './kanban.component.scss',
 })
 export class KanbanComponent implements OnInit {
   private readonly _getService = inject(GetService);
-  selectedWorkspace!: string | undefined;
+  selectedWorkspace!: Workspace;
   workspaces: Workspace[] = [];
   boards: Board[] = [];
-  selectedBoard!: string | undefined;
-
+  selectedBoard!: Board;
 
   ngOnInit() {
     this._getService.getAllWorkspace().subscribe((data: Workspace[]) => {
       this.workspaces = data;
       if (this.workspaces.length > 0) {
-        this.selectedWorkspace = this.workspaces[0].displayName;
-
-        this._getService.getAllBoards({organizations: this.workspaces[0].id}).subscribe((data: Board[]) => {
-          this.boards = data;
-          if (this.boards.length > 0) {
-            this.selectedBoard = this.boards[0].name;
-          }
-        })
+        this.selectedWorkspace = this.workspaces[0];
       }
+
+      this.updateBoards()
     });
+  }
+
+  updateBoards() {
+    if (this.selectedWorkspace) {
+      this._getService.getAllBoards({organizations: this.selectedWorkspace.id}).subscribe((data: Board[]) => {
+        this.boards = data;
+        if (this.boards.length > 0) {
+          this.selectedBoard = this.boards[0];
+        }
+      });
+    }
   }
 }
