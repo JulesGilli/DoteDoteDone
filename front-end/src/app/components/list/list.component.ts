33b9b4d1cf54875c9b0dcecd5d95ba60e1e56f18
JulesGilli <jules.gilli@epitech.eu>
2025-03-18 15:35:33 +0100

    feat: drag n drop in kandan with trello connection

--- front-end/src/app/components/list/list.component.ts
+++ front-end/src/app/components/list/list.component.ts
@@ -1,36 +1,75 @@
-import {Component, inject, Input} from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output, computed } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragMove, DropListRef} from '@angular/cdk/drag-drop';
 import { CardListComponent } from '../card-list/card-list.component';
-import {Card, List} from '../../models';
-import {UtilsService} from '../../services/utils/utils.service';
-import {SharedModule} from '../../../shared.module';
-import {ModalCreateComponent} from '../modal-create/modal-create.component';
+import { Card, List } from '../../models';
+import { UtilsService } from '../../services/utils/utils.service';
+import { SharedModule } from '../../../shared.module';
+import { ModalCreateComponent } from '../modal-create/modal-create.component';
+import { PostService } from '../../services';
+import { DragDropService } from '../../services/drag-drop.service';
 
 @Component({
   selector: 'app-list',
   imports: [CardListComponent, SharedModule, ModalCreateComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss',
+  styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
   @Input() list!: List;
+  @Output() cardDragMoved = new EventEmitter<CdkDragMove<any>>();
 
-  isEditMode: boolean = false;
-  isCreateMode: boolean = false;
-
+  isCreateMode = false;
   selectedTicket: any = null;
+  draggedCardId: string | null = null;
+  isDragging: DragDropService = inject(DragDropService);
 
   private readonly _utilsService = inject(UtilsService);
+  private readonly _postService = inject(PostService);
 
-  public getCardsForList(): Card[] {
-    return this._utilsService.tickets().filter((card) => card.idList === this.list.id);
-  }
+  cards = computed(() =>
+    this._utilsService.tickets().filter(card => card.idList === this.list.id && card.id !== this.draggedCardId)
+  );
 
-  openCreateModal() {
+  ngOnInit(): void {}
+
+  openCreateModal(): void {
     this.isCreateMode = true;
     this.selectedTicket = null;
   }
 
-  closeCreateModal() {
-    this.isCreateMode = false;
+  drop(event: CdkDragDrop<Card[]>): void {
+    this.draggedCardId = null;
+    const currentCards = this.cards();
+    if (event.previousContainer === event.container) {
+      moveItemInArray(currentCards, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      const movedCard = event.container.data[event.currentIndex];
+      movedCard.idList = this.list.id;
+      this._postService.updateCard(movedCard.id, movedCard).subscribe();
+    }
   }
+
+  onDragStarted(card: Card): void {
+    this.draggedCardId = card.id;
+    this.isDragging.setDragging(true);
+  }
+
+  onDragEnded(card: Card): void {
+    this.draggedCardId = null;
+    this.isDragging.setDragging(false);
+  }
+
+
+  onCardDragMoved(event: CdkDragMove<any>): void {
+    this.cardDragMoved.emit(event);
+  }
+
+  protected readonly DropListRef = DropListRef;
+  protected readonly DragDropService = DragDropService;
 }
--- front-end/src/app/pages/kanban/kanban.component.ts
+++ front-end/src/app/pages/kanban/kanban.component.ts
@@ -1,19 +1,41 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, AfterViewInit, inject} from '@angular/core';
+import { CdkDragMove } from '@angular/cdk/drag-drop';
+import { UtilsService } from '../../services/utils/utils.service';
 import { ListComponent } from '../../components/list/list.component';
 import { SharedModule } from '../../../shared.module';
-import { UtilsService } from '../../services/utils/utils.service';
-import {ModalCreateComponent} from '../../components/modal-create/modal-create.component';
 
 @Component({
   selector: 'app-kanban',
   imports: [ListComponent, SharedModule],
   templateUrl: 'kanban.component.html',
-  styleUrl: './kanban.component.scss',
+  styleUrls: ['./kanban.component.scss']
 })
-export class KanbanComponent implements OnInit {
+export class KanbanComponent implements OnInit, AfterViewInit {
   public readonly _utilsService = inject(UtilsService);
+
+  // Récupérer le conteneur Kanban via le template reference
+  @ViewChild('kanbanContainer') kanbanContainer!: ElementRef<HTMLDivElement>;
+
+  autoScrollThreshold = 100;
+  scrollSpeed = 10;
 
   ngOnInit(): void {
     this._utilsService.loadWorkspaces();
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  onDragMoved(event: CdkDragMove<any>): void {
+    const containerEl = this.kanbanContainer.nativeElement;
+    const containerRect = containerEl.getBoundingClientRect();
+    const pointerX = event.pointerPosition.x;
+
+    if (pointerX > containerRect.right - this.autoScrollThreshold) {
+      containerEl.scrollLeft += this.scrollSpeed;
+    }
+    if (pointerX < containerRect.left + this.autoScrollThreshold) {
+      containerEl.scrollLeft -= this.scrollSpeed;
+    }
   }
 }
