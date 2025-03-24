8ae84a6c3c9a4b9b5f69b983b7851e9e0f2f7c8b
JulesGilli <jules.gilli@epitech.eu>
2025-03-24 12:00:28 +0100

    feat: addind options button on lists to delete and move it

--- front-end/src/app/components/list/list.component.ts
+++ front-end/src/app/components/list/list.component.ts
@@ -1,31 +1,69 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output, computed } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragMove, DropListRef} from '@angular/cdk/drag-drop';
-import { CardListComponent } from '../card-list/card-list.component';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+  computed,
+  HostListener,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDragMove,
+  DropListRef,
+  CdkDropList,
+  CdkDrag,
+  CdkDragPlaceholder,
+  CdkDragPreview
+} from '@angular/cdk/drag-drop';
 import { Card, List } from '../../models';
 import { UtilsService } from '../../services/utils/utils.service';
-import { SharedModule } from '../../../shared.module';
-import { ModalCreateComponent } from '../modal-create/modal-create.component';
 import { PostService } from '../../services';
 import { DragDropService } from '../../services/drag-drop.service';
-
+import { FormsModule } from '@angular/forms';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { CardListComponent } from '../card-list/card-list.component';
 
 @Component({
   selector: 'app-list',
-  imports: [CardListComponent, SharedModule],
+  standalone: true,
   templateUrl: './list.component.html',
+  imports: [
+    CdkDropList,
+    AsyncPipe,
+    NgIf,
+    NgForOf,
+    CdkDrag,
+    CardListComponent,
+    CdkDragPreview,
+    CdkDragPlaceholder,
+    FormsModule
+  ],
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
   @Input() list!: List;
   @Output() cardDragMoved = new EventEmitter<CdkDragMove<any>>();
+  @Output() moveListEvent = new EventEmitter<{ list: List; direction: 'left' | 'right' }>();
+  @Output() deleteListEvent = new EventEmitter<string>();
 
   isCreateMode = false;
   selectedTicket: any = null;
   draggedCardId: string | null = null;
   isDragging: DragDropService = inject(DragDropService);
 
+  isEditingTitle = false;
+  isMenuOpen = false;
+
   private readonly _utilsService = inject(UtilsService);
   private readonly _postService = inject(PostService);
+
+  @ViewChild('dropdownContainer', { static: false }) dropdownContainer!: ElementRef;
 
   cards = computed(() =>
     this._utilsService.tickets().filter(card => card.idList === this.list.id && card.id !== this.draggedCardId)
@@ -71,6 +109,39 @@ export class ListComponent implements OnInit {
     this.cardDragMoved.emit(event);
   }
 
-  protected readonly DropListRef = DropListRef;
-  protected readonly DragDropService = DragDropService;
+  editTitleMode(): void {
+    this.isEditingTitle = true;
+  }
+
+  saveTitle(): void {
+    this.isEditingTitle = false;
+    this._postService.updateList(this.list.id, { name: this.list.name }).subscribe(
+      (updatedList) => {
+        this.list = updatedList;
+      }
+    );
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  deleteList(): void {
+    this._postService.updateList(this.list.id, { closed: true }).subscribe(() => {
+      this.deleteListEvent.emit(this.list.id);
+    });
+    this.isMenuOpen = false;
+  }
+
+  moveList(direction: 'left' | 'right'): void {
+    this.moveListEvent.emit({ list: this.list, direction });
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.dropdownContainer && !this.dropdownContainer.nativeElement.contains(event.target)) {
+      this.isMenuOpen = false;
+    }
+  }
 }
--- front-end/src/app/pages/kanban/kanban.component.ts
+++ front-end/src/app/pages/kanban/kanban.component.ts
@@ -1,8 +1,10 @@
 import {Component, ElementRef, OnInit, ViewChild, AfterViewInit, inject} from '@angular/core';
 import { CdkDragMove } from '@angular/cdk/drag-drop';
+import { List } from '../../models';
 import { ListComponent } from '../../components/list/list.component';
 import { SharedModule } from '../../../shared.module';
 import { UtilsService } from '../../services/utils/utils.service';
+import { PostService } from '../../services';
 
 @Component({
   selector: 'app-kanban',
@@ -12,8 +14,8 @@ import { UtilsService } from '../../services/utils/utils.service';
 })
 export class KanbanComponent implements OnInit, AfterViewInit {
   public readonly _utilsService = inject(UtilsService);
+  private readonly _postService = inject(PostService);
 
-  // Récupérer le conteneur Kanban via le template reference
   @ViewChild('kanbanContainer') kanbanContainer!: ElementRef<HTMLDivElement>;
 
   autoScrollThreshold = 100;
@@ -37,5 +39,54 @@ export class KanbanComponent implements OnInit, AfterViewInit {
     if (pointerX < containerRect.left + this.autoScrollThreshold) {
       containerEl.scrollLeft -= this.scrollSpeed;
     }
+  }
+
+  onMoveList(event: { list: List; direction: 'left' | 'right' }): void {
+    const boardId = this._utilsService.selectedBoard()?.id;
+    if (!boardId) {
+      return;
+    }
+    this._utilsService.lists.update(prev => {
+      const boardLists = prev[boardId] || [];
+      const currentIndex = boardLists.findIndex(l => l.id === event.list.id);
+      if (currentIndex === -1) {
+        return prev;
+      }
+      let newIndex = currentIndex;
+      if (event.direction === 'left' && currentIndex > 0) {
+        newIndex = currentIndex - 1;
+      } else if (event.direction === 'right' && currentIndex < boardLists.length - 1) {
+        newIndex = currentIndex + 1;
+      }
+      if (newIndex !== currentIndex) {
+        const updatedLists = [...boardLists];
+        const [movedList] = updatedLists.splice(currentIndex, 1);
+        updatedLists.splice(newIndex, 0, movedList);
+        updatedLists.forEach((l, index) => {
+          l.pos = index;
+          this._postService.updateList(l.id, { pos: index }).subscribe();
+        });
+        return {
+          ...prev,
+          [boardId]: updatedLists
+        };
+      }
+      return prev;
+    });
+  }
+
+  onDeleteList(deletedListId: string): void {
+    const boardId = this._utilsService.selectedBoard()?.id;
+    if (!boardId) {
+      return;
+    }
+    this._utilsService.lists.update(prev => {
+      const boardLists = prev[boardId] || [];
+      const updatedLists = boardLists.filter(l => l.id !== deletedListId);
+      return {
+        ...prev,
+        [boardId]: updatedLists
+      };
+    });
   }
 }
