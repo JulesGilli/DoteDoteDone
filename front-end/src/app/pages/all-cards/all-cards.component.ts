c1d0120de7a8a63d3d3846d12d99015b3a68ee8d
JulesGilli <jules.gilli@epitech.eu>
2025-04-01 17:06:39 +0200

    Revert "Merge pull request #66 from EpitechMscProPromo2027/all-cards"
    This reverts commit 2aeb6066c9af0e06e38a7434807e5a2b0e083ff6, reversing
    changes made to 58ca456b3c0ef3b5177cefe4cdb486190f3a30de.

--- front-end/src/app/pages/all-cards/all-cards.component.ts
+++ front-end/src/app/pages/all-cards/all-cards.component.ts
@@ -74,14 +74,6 @@ export class AllCardsComponent implements OnInit {
   }
 
   async loadCards() {
-    const allBoards = Object.values(this._dataService.allBoards());
-    if (allBoards.length === 0) {
-      setTimeout(() => this.loadCards(), 500);
-      console.log("no boards");
-      return;
-    }
-    
-
     this.loading = true;
     if (this.selectedWorkspace.id === 'all' && !this.allTickets['all']) {
       for (const board of Object.values(this._dataService.allBoards()).flat()) {
@@ -95,7 +87,9 @@ export class AllCardsComponent implements OnInit {
           .filter((c) => boards.some((b) => b.id === c.idBoard));
       }
 
-      this.allTickets['all'] = Object.values(this._dataService.allTickets()).flat();
+      this.allTickets['all'] = Object.values(
+        this._dataService.allTickets()
+      ).flat();
       this.tickets = this.formatOfTickets(this.allTickets['all']);
       this.loading = false;
     } else {
@@ -106,45 +100,13 @@ export class AllCardsComponent implements OnInit {
     }
   }
 
-
-  // loadCardsFromBoard(boards: Board[]): void {
-  //   const cardsObservables = boards.map((board) =>
-  //     this._getService.getAllCards({ boards: board.id })
-  //   );
-  //   forkJoin(cardsObservables).subscribe({
-  //     next: (cardsArrays: Card[][]) => {
-  //       const allCards = ([] as Card[]).concat(...cardsArrays);
-  //       this.allTickets[this.selectedWorkspace.id] = allCards;
-  //       this.tickets = this.formatOfTickets(allCards);
-  //       this.loading = false;
-  //     },
-  //     error: (err) => {
-  //       console.error(err);
-  //       this.error = err;
-  //       this.loading = false;
-  //     },
-  //   });
-  // }
-
   formatOfTickets(cards: Card[]): any[] {
-    return cards.map((card) => {
-      let workspaceName = 'Unknown Workspace';
-      const allBoardsRecord = this._dataService.allBoards();
-      for (const [wsId, boardList] of Object.entries(allBoardsRecord)) {
-        if (boardList.some((board: Board) => board.id === card.idBoard)) {
-          const ws = this.workspaces.find((ws) => ws.id === wsId);
-          if (ws) {
-            workspaceName = ws.displayName ?? 'Unknown Workspace';
-          }
-          break;
-        }
-      }
-
+    const tickets = cards.map((card) => {
       const ticket = {
         titre: card.name,
         resume: card.desc,
         statusCard: 'normal',
-        ticketId: workspaceName,
+        ticketId: card.id,
         manager: 'No one',
       };
 
@@ -159,6 +121,7 @@ export class AllCardsComponent implements OnInit {
       }
       return ticket;
     });
+    return tickets;
   }
 
   async getMembers(idMember: string): Promise<string> {
