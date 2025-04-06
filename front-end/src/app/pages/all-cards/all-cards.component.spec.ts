ca3f05242fe0be1c299646128e9c558a47f44272
ileanasaintpaul <ileana.saint-paul@epitech.eu>
2025-04-06 18:19:47 +0200

    feat: add tests

--- front-end/src/app/pages/all-cards/all-cards.component.spec.ts
+++ front-end/src/app/pages/all-cards/all-cards.component.spec.ts
@@ -1,7 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { AllCardsComponent } from './all-cards.component';
 import {HttpClientModule} from '@angular/common/http';
+import {of} from 'rxjs';
+import {Card} from '../../models';
+import {GetService} from '../../services';
 
 describe('AllCardsComponent', () => {
   let component: AllCardsComponent;
@@ -20,5 +23,62 @@ describe('AllCardsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open modal with selected ticket data', () => {
+    const getService = TestBed.inject(GetService);
+    const fakeTicket = {
+      idBoard: '13',
+      ticketId: '12',
+      name: 'Test Ticket',
+      desc: 'Test Desc',
+    };
+
+    spyOn(getService, 'getBoardById').and.returnValue(of({
+      id: 'board1',
+      name: 'Board Name',
+      idOrganization: 'org1'
+    }));
+
+    spyOn(getService, 'getWorkspaceById').and.returnValue(of({
+      id: 'org1',
+      displayName: 'Workspace Test'
+    }));
+
+    component.openModal(fakeTicket);
+
+    expect(component.isEditMode).toBeTrue();
+  });
+
+  it('should return the correct format for a card', () => {
+    const getService = TestBed.inject(GetService);
+    const mockCard: Card = {
+      id: '1',
+      name: 'Display infos',
+      desc: 'Ticket to display infos',
+      idBoard: '2',
+      idMembers: ['member']
+    } as Card;
+
+    spyOn(getService, 'getBoardById').and.returnValue(of({
+      id: 'board1',
+      name: 'Board Name',
+      idOrganization: 'org1'
+    }));
+
+    spyOn(getService, 'getWorkspaceById').and.returnValue(of({
+      id: 'org1',
+      displayName: 'Workspace Test'
+    }));
+
+    component.membersFromTicket['member'] = 'Sayu';
+
+    const formattedTickets = component.formatOfTickets([mockCard]);
+
+    expect(formattedTickets.length).toBe(1);
+    expect(formattedTickets[0].name).toBe('Display infos');
+    expect(formattedTickets[0].desc).toBe('Ticket to display infos');
+    expect(formattedTickets[0].ticketId).toBe('1');
+    expect(formattedTickets[0].manager).toBe('Sayu');
   });
 });
